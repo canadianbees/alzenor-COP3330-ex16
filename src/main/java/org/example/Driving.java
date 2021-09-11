de21c87610c75564f30008b90b81f4a7faf11d5a
Celina Alzenor <you@example.com>
2021-09-11 18:09:28 -0400

    modification

--- src/main/java/org/example/Driving.java
+++ src/main/java/org/example/Driving.java
@@ -20,17 +20,22 @@ public class Driving
 
     public void VerifyAge()
     {
-        //if age is 16 or greater, the person can drive
-        if(this.age >= 16)
+
+        //decide which case we have based on the persons age
+        int result = this.age >= 16 ? 1 : 2;
+
+        //if the person is older than or is 16
+        if(result == 1)
         {
             System.out.print("You are old enough to legally drive.");
-            return;
         }
 
-        //if less, they can not drive
-        System.out.print("You are not old enough to legally drive.");
+        //if they are younger
+        if(result == 2)
+        {
+            System.out.print("You are not old enough to legally drive.");
+        }
+
     }
-
-
 
 }
